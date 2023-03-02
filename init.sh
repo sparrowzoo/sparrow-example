1c5f8edcae8693a9f860a36babb3fff0d0023694
harry <zh_harry@163.com>
2023-03-02 17:21:58 +0800

    init 脚手架

---
+++ init.sh
@@ -0,0 +1,16 @@
+rm -rf admin/example-admin-adapter/src
+rm -rf admin/example-admin-dao-api/src
+rm -rf admin/example-admin-dao-sparrow/src
+rm -rf admin/example-admin-domain/src
+rm -rf admin/example-admin-infrastructure/src
+rm -rf admin/example-admin-protocol/src
+
+rm -rf admin/example-admin-adapter/target
+rm -rf admin/example-admin-dao-api/target
+rm -rf admin/example-admin-dao-sparrow/target
+rm -rf admin/example-admin-domain/target
+rm -rf admin/example-admin-infrastructure/target
+rm -rf admin/example-admin-protocol/target
+rm -rf admin/example-admin-main-spring-boot/src/main/resources/templates/sparrow*
+
+
